789ec1d1a7f011dcfdae19af1303acbe5c59a54d
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-09-06 23:21:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
 import torch
 import timm
 
-# m = timm.create_model('divervit_base_patch16_224', pretrained=False)
-# m(torch.rand([4, 3, 224, 224]))
-
-a = torch.ones([2, 3])
-print('==== a ====\n{}'.format(a))
-b = a
-b[0][1] = 10
-print('==== a ====\n{}'.format(a))
-print('==== b ====\n{}'.format(b))
+m = timm.create_model('divervit_base_patch16_224', pretrained=False)
+m(torch.rand([4, 3, 224, 224]))
