a98d9b958ae6e5f107aab7d76c638156123beba3
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-09-07 15:44:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import torch
 import timm
 
-input1 = torch.randn(5, 128)
-input2 = torch.randn(5, 128)
+input1 = torch.randn(2, 5, 128)
+input2 = torch.randn(2, 5, 128)
 cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
 output = cos(input1, input2)
 print(output)
