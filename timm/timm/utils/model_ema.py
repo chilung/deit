8361e7b59941e11dc2f1e857af48a2a533923929
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-09-07 11:07:45 +0800

    Update model_ema.py

--- timm/timm/utils/model_ema.py
+++ timm/timm/utils/model_ema.py
@@ -77,7 +77,8 @@ class ModelEma:
                 model_v = msd[k].detach()
                 if self.device:
                     model_v = model_v.to(device=self.device)
-                ema_v.copy_(ema_v * self.decay + (1. - self.decay) * model_v)
+                if ema_v.shape[0] != 0:
+                    ema_v.copy_(ema_v * self.decay + (1. - self.decay) * model_v)
 
 
 class ModelEmaV2(nn.Module):
