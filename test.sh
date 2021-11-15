db2292c2ff9ef636d1ebabbeff093624aa41d133
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-11-15 10:17:15 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,6 @@
- python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model divervit_disable_d32_patch32_dim192_h6_r3_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_disable_d32_patch32_dim192_h6_r3_224/  --num_workers=8 --epochs=500 --divervit_alpha=0
-
-
- python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model divervit_enable_d32_patch32_dim192_h6_r3_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_enable_head_diversity_d32_patch32_dim192_h6_r3_224_correct_loss_0.1/  --num_workers=8 --epochs=500 --divervit_alpha=0.1
-
-
- python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model divervit_enable_d32_patch32_dim192_h6_r3_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_enable_head_diversity_d32_patch32_dim192_h6_r3_224_correct_loss_0.05/  --num_workers=8 --epochs=500 --divervit_alpha=0.05
-
-
- python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model divervit_enable_d32_patch32_dim192_h6_r3_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_enable_head_diversity_d32_patch32_dim192_h6_r3_224_correct_loss_0.01/  --num_workers=8 --epochs=500 --divervit_alpha=0.01
-
-
- python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model divervit_enable_d32_patch32_dim192_h6_r3_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_enable_head_diversity_d32_patch32_dim192_h6_r3_224_correct_loss_0.005/  --num_workers=8 --epochs=500 --divervit_alpha=0.005
-
-
- python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model divervit_enable_d32_patch32_dim192_h6_r3_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_enable_head_diversity_d32_patch32_dim192_h6_r3_224_correct_loss_0.001/  --num_workers=8 --epochs=500 --divervit_alpha=0.001
-
-
- python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model divervit_enable_d32_patch32_dim192_h6_r3_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_enable_head_diversity_d32_patch32_dim192_h6_r3_224_correct_loss_0.0005/  --num_workers=8 --epochs=500 --divervit_alpha=0.0005
-
-
- python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py --model divervit_enable_d32_patch32_dim192_h6_r3_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_enable_head_diversity_d32_patch32_dim192_h6_r3_224_correct_loss_0.0001/  --num_workers=8 --epochs=500 --divervit_alpha=0.0001
+python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --model divervit_base_patch16_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_base_patch16_224_alpha_0.1/  --num_workers=8 --epochs=500 --divervit_alpha=0.1
+python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --model divervit_d18_patch16_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_d18_patch16_224_alpha_0.1/  --num_workers=8 --epochs=500 --divervit_alpha=0.1
+python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --model divervit_d24_patch16_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_d24_patch16_224_alpha_0.1/  --num_workers=8 --epochs=500 --divervit_alpha=0.1
+python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --model divervit_d32_patch16_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_divervit_d32_patch16_224_alpha_0.1/  --num_workers=8 --epochs=500 --divervit_alpha=0.1
+python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --model deit_base_patch16_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_deit_base_patch16_224/  --num_workers=8 --epochs=500 --divervit_alpha=0.0
+python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --model deit_d32_patch16_224 --batch-size 256 --data-path ../Dataset/imagenet/ILSVRC/Data/CLS-LOC --output_dir ./checkpoint_deit_d32_patch16_224/  --num_workers=8 --epochs=500 --divervit_alpha=0.0
